4c79c74de80bfdd34a1dcc4a5e42d1d09049c5ff
Alex Pariseau <arpariseau@gmail.com>
2020-05-22 11:17:31 -0600

    Can now add an actor through a form on the movie show page.

---
+++ app/controllers/movie_actors_controller.rb
@@ -0,0 +1,8 @@
+class MovieActorsController < ApplicationController
+  def create
+    movie = Movie.find(params[:id])
+    actor = Actor.find_by(name: params[:actor_name])
+    MovieActor.create(movie_id: movie.id, actor_id: actor.id)
+    redirect_to "/movies/#{movie.id}"
+  end
+end
