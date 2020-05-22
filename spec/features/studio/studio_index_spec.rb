1dd5359d46228ef8b5fd4469b16aa539a0c61ecd
Alex Pariseau <arpariseau@gmail.com>
2020-05-22 09:29:41 -0600

    Migration for movie and studio dbs

--- spec/features/studio/studio_index_spec.rb
+++ spec/features/studio/studio_index_spec.rb
@@ -1,35 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe "Studio index page", type: :feature do
-  mgm = Studio.create(name: "Metro Goldywn Meyer",
-                      location: "Beverly Hills, CA")
-  fox = Studio.create(name: "20th Century Fox",
-                      location: "Los Angeles, CA")
-  ben_hur = Movie.create(title: "Ben-Hur",
-                         year: 1959,
-                         genre: "epic",
-                         studio_id: mgm.id)
-  odyssey = Movie.create(title: "2001: A Space Odyssey",
-                         year: 1968,
-                         genre: "sci-fi",
-                         studio_id: mgm.id)
-   apes = Movie.create(title: "Planet of the Apes",
-                       year: 1968,
-                       genre: "sci-fi",
-                       studio_id: fox.id)
+  it "can list all movie studios" do
+    mgm = Studio.create(name: "Metro Goldywn Meyer",
+                        location: "Beverly Hills, CA")
+    fox = Studio.create(name: "20th Century Fox",
+                        location: "Los Angeles, CA")
+    ben_hur = Movie.create(title: "Ben-Hur",
+                           year: 1959,
+                           genre: "epic",
+                           studio_id: mgm.id)
+    odyssey = Movie.create(title: "2001: A Space Odyssey",
+                           year: 1968,
+                           genre: "sci-fi",
+                           studio_id: mgm.id)
+    apes = Movie.create(title: "Planet of the Apes",
+                        year: 1968,
+                        genre: "sci-fi",
+                        studio_id: fox.id)
 
-   visit "/studios"
+    visit "/studios"
 
-   within(".studio-#{mgm.id}") do
-     expect(page).to have_content(mgm.name)
-     expect(page).to have_content(ben_hur.name)
-     expect(page).to have_content(odyssey.name)
-   end
+    within(".studio-#{mgm.id}") do
+      expect(page).to have_content(mgm.name)
+      expect(page).to have_content(ben_hur.name)
+      expect(page).to have_content(odyssey.name)
+    end
 
-   within(".studio-#{fox.id}") do
-     expect(page).to have_content(fox.name)
-     expect(page).to have_content(apes.name)
-   end
+    within(".studio-#{fox.id}") do
+      expect(page).to have_content(fox.name)
+      expect(page).to have_content(apes.name)
+    end
+  end
 end
 
 # Story 1
